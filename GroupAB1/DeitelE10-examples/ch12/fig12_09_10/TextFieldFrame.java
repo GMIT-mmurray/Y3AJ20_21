597132d95e761b2c79519e5906b3a7f19fc64ccb
mmurray <michael.murray@gmit.ie>
2020-10-01 20:50:01 +0100

    IntelliJ

--- GroupAB1/DeitelE10-examples/ch12/fig12_09_10/TextFieldFrame.java
+++ GroupAB1/DeitelE10-examples/ch12/fig12_09_10/TextFieldFrame.java
@@ -12,7 +12,7 @@ public class TextFieldFrame extends JFrame
 {
    private final JTextField textField1; // text field with set size
    private final JTextField textField2; // text field with text
-   private  JTextField textField3; // text field with text and size
+   private final JTextField textField3; // text field with text and size
    private final JPasswordField passwordField; // password field with text
 
    // TextFieldFrame constructor adds JTextFields to JFrame
@@ -23,65 +23,51 @@ public class TextFieldFrame extends JFrame
 
       // construct textfield with 10 columns
       textField1 = new JTextField(10);
-      textField1.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent event) {
-            String string = String.format("%s", event.getActionCommand());
-            JOptionPane.showMessageDialog(null, string);
-            if (string.equals("three")) {
-               textField3.setEditable(true);
-               textField3.setText("I am alive");
-            }
-         }
+      textField1.addActionListener(e -> {
+         String string = String.format("%s",
+                 e.getActionCommand());
+         JOptionPane.showMessageDialog(null, string);
       });
       add(textField1); // add textField1 to JFrame
 
       // construct textfield with default text
       textField2 = new JTextField("Enter text here");
+
       textField2.addActionListener(e -> {
-         String string = String.format("%s", e.getActionCommand());
-         JOptionPane.showMessageDialog(null, string);
-         if (string.equals("THREE"))
-            textField3.setEditable(false);
-      });add(textField2); // add textField2 to JFrame
+            String string = String.format("textField2: %s",
+                    e.getActionCommand());
+            JOptionPane.showMessageDialog(null, string);
+
+      }); add(textField2); // add textField2 to JFrame
 
       // construct textfield with default text and 21 columns
       textField3 = new JTextField("Uneditable text field", 21);
-      textField3.setEditable(false); // disable editing
       textField3.addActionListener(new ActionListener() {
          @Override
          public void actionPerformed(ActionEvent e) {
-            String string = String.format("%s", e.getActionCommand());
+            String string = String.format("textField3: %s",
+                    e.getActionCommand());
             JOptionPane.showMessageDialog(null, string);
          }
-      }); add(textField3); // add textField3 to JFrame
+      });
+      textField3.setEditable(false); // disable editing
+      add(textField3); // add textField3 to JFrame
 
       // construct passwordfield with default text
       passwordField = new JPasswordField("Hidden text");
       passwordField.addActionListener(new ActionListener() {
          @Override
          public void actionPerformed(ActionEvent e) {
-            String string = String.format("%s", e.getActionCommand());
+            String string = String.format("%s",
+                    e.getActionCommand());
+            if (string.equals("pass")) {
+               textField3.setEditable(true);
+            }
             JOptionPane.showMessageDialog(null, string);
          }
-      });
-      add(passwordField); // add passwordField to JFrame
+      });   add(passwordField); // add passwordField to JFrame
 
    } // end TextFieldFrame constructor
 
 } // end class TextFieldFrame
 
-/**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
