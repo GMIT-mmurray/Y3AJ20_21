ff73807ab9e6a1d396380bcbd75c6971d956e5dc
mmurray <michael.murray@gmit.ie>
2020-10-05 09:36:48 +0100

    Mon 5/10/2020 #Button

--- GroupB2C/DeitelE10-examples/ch12/fig12_15_16/ButtonFrame.java
+++ GroupB2C/DeitelE10-examples/ch12/fig12_15_16/ButtonFrame.java
@@ -9,44 +9,43 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
-public class ButtonFrame extends JFrame 
-{
+public class ButtonFrame extends JFrame {
    private final JButton plainJButton; // button with just text
    private final JButton fancyJButton; // button with icons
 
    // ButtonFrame adds JButtons to JFrame
-   public ButtonFrame()
-   {
+   public ButtonFrame() {
       super("Testing Buttons");
-      setLayout(new FlowLayout()); 
+      setLayout(new FlowLayout());
 
       plainJButton = new JButton("Plain Button"); // button with text
       add(plainJButton); // add plainJButton to JFrame
 
       Icon bug1 = new ImageIcon(getClass().getResource("bug1.gif"));
       Icon bug2 = new ImageIcon(getClass().getResource("bug2.gif"));
-      fancyJButton = new JButton("Fancy Button", bug1); // set image
+      fancyJButton = new JButton("Fancy Button", bug1); // set string/image
       fancyJButton.setRolloverIcon(bug2); // set rollover image
       add(fancyJButton); // add fancyJButton to JFrame
 
       // create new ButtonHandler for button event handling 
-      ButtonHandler handler = new ButtonHandler();
-      fancyJButton.addActionListener(handler);
-      plainJButton.addActionListener(handler);
-   }
 
-   // inner class for button event handling
-   private class ButtonHandler implements ActionListener 
-   {
-      // handle button event
-      @Override
-      public void actionPerformed(ActionEvent event)
-      {
-         JOptionPane.showMessageDialog(ButtonFrame.this, String.format(
-            "You pressed: %s", event.getActionCommand()));
-      }
-   } 
-} // end class ButtonFrame
+      fancyJButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(ButtonFrame.this, String.format(
+                    "You pressed: %s", e.getActionCommand()));
+         }
+      });
+      plainJButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(ButtonFrame.this, String.format(
+                    "You pressed: %s", e.getActionCommand()));
+         }
+      });
+   }
+}
+
 
 /**************************************************************************
  * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
