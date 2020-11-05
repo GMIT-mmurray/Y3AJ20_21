de0a313e9f3d6b6990deeadbfeb5a3818e7f8ffc
mmurray <michael.murray@gmit.ie>
2020-11-05 10:53:15 +0000

    Final version

--- GroupB2C/JavaFundamentals/Lesson07/Activity29Grades/IterateOnGradesVector.java
+++ GroupB2C/JavaFundamentals/Lesson07/Activity29Grades/IterateOnGradesVector.java
@@ -5,8 +5,11 @@ public class IterateOnGradesVector {
 
     public static void main(String [] args) throws IOException {
         Vector<Grade> students = GradesLoader.loadGradesInVector(args[0]);
-        for (Grade gradeAsObject : students) {
-        //    System.out.printf("%s - %s\n", gradeAsObject.name, gradeAsObject.email);
+        for (Grade student : students) {
+            System.out.printf("%s %s %s %.2f %.2f %.2f %.2f Av %.2f\n",
+                               student.firestname, student.lastName, student.SSN,
+                               student.test1,student.test2,student.test3,student.test4,
+                               (student.test1+student.test2+student.test3+student.test4)/4);
         }
     }
 
