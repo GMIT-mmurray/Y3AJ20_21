08fb8909c4e9120d385a1e227bdf0536e8e241a3
mmurray <michael.murray@gmit.ie>
2020-11-05 09:47:25 +0000

    5/11/2020 - Code for Grade.csv after code editing #1

--- GroupB2C/JavaFundamentals/Lesson07/Activity29Grades/IterateOnGradesVector.java
+++ GroupB2C/JavaFundamentals/Lesson07/Activity29Grades/IterateOnGradesVector.java
@@ -4,9 +4,9 @@ import java.util.Vector;
 public class IterateOnGradesVector {
 
     public static void main(String [] args) throws IOException {
-        Vector<Grade> users = GradesLoader.loadGradesInVector(args[0]);
-        for (Grade gradeAsObject : users) {
-            System.out.printf("%s - %s\n", gradeAsObject.name, gradeAsObject.email);
+        Vector<Grade> students = GradesLoader.loadGradesInVector(args[0]);
+        for (Grade gradeAsObject : students) {
+        //    System.out.printf("%s - %s\n", gradeAsObject.name, gradeAsObject.email);
         }
     }
 
