912b30b95966d17ceb3224a127269ca48e79ebc6
mmurray <michael.murray@gmit.ie>
2020-11-26 09:58:12 +0000

    26/11/2020 10am

--- GroupB2C/Java8/src/Main.java
+++ GroupB2C/Java8/src/Main.java
@@ -3,12 +3,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> pointList = new ArrayList();
+/*        List<String> pointList = new ArrayList();
 
         pointList.add("1");
         pointList.add("2");
@@ -19,16 +20,17 @@ public class Main {
 
         JButton button = new JButton("Submit");
         button.addActionListener(event -> System.out.println("Button Pressed"));
-     /**************************************************************************/
+     *//**************************************************************************//*
         Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
         stream.forEach(p -> System.out.println(p));
+        stream.forEach(System.out::println);
 
-     /*************************************************************************/
+     *//*************************************************************************//*
 
         Stream<Integer> stream1 = Stream.of( new Integer[]{1,2,3,4,5,6,7,8,9} );
         stream1.forEach(p -> System.out.println(p));
 
-     /*************************************************************************/
+     *//*************************************************************************//*
         List<Integer> list = new ArrayList<Integer>();
 
         for(int i = 1; i< 10; i++){
@@ -37,6 +39,17 @@ public class Main {
 
         Stream<Integer> stream2 = list.stream();
         stream.forEach(p -> System.out.println(p));
+        *//*************************************************************************/
+
+        Stream.generate(() -> (new Random()).nextInt(100)).limit(20).forEach(System.out::println);
+
+
+
     }
+    /*************************************************************************/
+
+
+
+
 }
 
