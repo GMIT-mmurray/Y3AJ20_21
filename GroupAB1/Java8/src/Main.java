f6b56b97c69e9a6317be9fb490177106c8f27162
mmurray <michael.murray@gmit.ie>
2020-11-30 16:46:49 +0000

    30/11/2020 5pm code

--- GroupAB1/Java8/src/Main.java
+++ GroupAB1/Java8/src/Main.java
@@ -66,7 +66,59 @@ public class Main implements MyFirstFunctionalInterface{
                                         .filter(i -> i%2 == 0)
                                         .toArray(Integer[]::new);
 
-        System.out.print(evenNumbersArr);
+       // System.out.print(evenNumbersArr);
+
+        List<String> memberNames = new ArrayList<>();
+        memberNames.add("Amitabh");
+        memberNames.add("Shekhar");
+        memberNames.add("Aman");
+        memberNames.add("Rahul");
+        memberNames.add("Shahrukh");
+        memberNames.add("Salman");
+        memberNames.add("Yana");
+        memberNames.add("Lokesh");
+
+        memberNames.stream()
+                   .filter((s) -> s.startsWith("A"))
+                   .forEach(System.out::println);
+
+        memberNames.stream()
+                .filter((s) -> s.startsWith("A"))
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+
+        memberNames.stream()
+                   .sorted()
+                   .map(String::toUpperCase)
+                   .forEach(System.out::println);
+
+        List<String> memNamesInUppercase = memberNames.stream()
+                                                      .sorted()
+                                                      .map(String::toUpperCase)
+                                                      .collect(Collectors.toList());
+
+        System.out.print(memNamesInUppercase);
+        boolean matchedResult = memberNames.stream()
+                                           .anyMatch((s) -> s.startsWith("A"));
+
+        System.out.println(matchedResult);
+
+        matchedResult = memberNames.stream()
+                                   .allMatch((s) -> s.startsWith("A"));
+
+        System.out.println(matchedResult);
+
+        matchedResult = memberNames.stream()
+                                   .noneMatch((s) -> s.startsWith("A"));
+
+        System.out.println(matchedResult);
+
+        long totalMatched = memberNames.stream()
+                                       .filter((s) -> s.startsWith("A"))
+                                       .count();
+
+        System.out.println(totalMatched);
+
 
     }
 
